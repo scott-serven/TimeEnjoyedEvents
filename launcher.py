26950f2090a3ab2fb228f7efef3a78abd7a6027a
EvieePy <evieepy@gmail.com>
2023-05-16 07:15:25 +1000

    Add universal launcher.py

---
+++ launcher.py
@@ -0,0 +1,47 @@
+"""
+MIT License
+
+Copyright (c) 2023 EvieePy
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+import asyncio
+
+import aiohttp
+import uvicorn
+
+import api
+import universal
+from bot.core import Bot
+
+
+async def main() -> None:
+
+    async with aiohttp.ClientSession() as session:
+        async with Bot(session=session) as bot:
+            bot_: asyncio.Task = asyncio.create_task(bot.start(universal.CONFIG['TOKENS']['bot']))
+
+            config: uvicorn.Config = uvicorn.Config("__main__:app", port=universal.CONFIG['SERVER']['port'])
+            server: uvicorn.Server = uvicorn.Server(config)
+            await server.serve()
+
+
+if __name__ == '__main__':
+    app: api.Server = api.Server()
+    asyncio.run(main())
