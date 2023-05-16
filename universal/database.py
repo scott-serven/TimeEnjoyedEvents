b20cf6533c04e7dccdd2cc8b26b1f5d99b6e6997
EvieePy <evieepy@gmail.com>
2023-05-17 08:35:45 +1000

    Add basic team methods to database.py

--- universal/database.py
+++ universal/database.py
@@ -22,6 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 import logging
+import secrets
 import tomllib
 from typing import Any, Self
 
@@ -68,3 +69,50 @@ class Database:
         logger.info('Completed Database Setup.')
 
         return self_
+
+    async def create_team(
+            self,
+            *,
+            name: str,
+            owner: int,
+            role_id: int,
+            text_id: int,
+            voice_id: int
+    ) -> None:
+        id_: int = secrets.randbits(32)
+        token: str = secrets.token_urlsafe(32)
+        invite: str = secrets.token_urlsafe(4)
+
+        query: str = """INSERT INTO teams(id, token, invite, name, owner, role_id, text_id, voice_id)
+                        VALUES ($1, $2, $3, $4, $5, $6, $7, $8)"""
+        async with self._pool.acquire() as connection:
+            await connection.execute(query, id_, token, invite, name, owner, role_id, text_id, voice_id)
+
+    async def fetch_team(
+            self,
+            *,
+            id_: int | None = None,
+            token: str | None = None,
+            invite: str | None = None,
+            owner: int | None = None,
+            name: str | None = None,
+            text_id: int | None = None,
+            role_id: int | None = None
+    ) -> asyncpg.Record:
+
+        query: str = """
+        SELECT * FROM teams WHERE
+            id = $1 OR token = $2 OR invite = $3 OR owner = $4 OR name = $5 OR text_id = $6 OR role_id = $7
+            """
+        async with self._pool.acquire() as connection:
+            row: asyncpg.Record = await connection.fetchrow(query, id_, token, invite, owner, name, text_id, role_id)
+
+        return row
+
+    async def fetch_teams(self) -> list[asyncpg.Record]:
+        query: str = """SELECT * FROM teams"""
+
+        async with self._pool.acquire() as connection:
+            rows: list[asyncpg.Record] = await connection.fetch(query)
+
+        return rows
