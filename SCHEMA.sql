2a580079c53940b77df04ae397591eeffdc0b363
EvieePy <evieepy@gmail.com>
2023-05-17 08:33:57 +1000

    Create teams schema

--- SCHEMA.sql
+++ SCHEMA.sql
@@ -1,3 +1,12 @@
-CREATE TABLE IF NOT EXISTS testing(
-    uuid SERIAL PRIMARY KEY
+CREATE TABLE IF NOT EXISTS teams(
+    id BIGINT PRIMARY KEY,
+    token TEXT UNIQUE NOT NULL,
+    invite TEXT UNIQUE NOT NULL,
+    name TEXT UNIQUE NOT NULL,
+    github TEXT,
+    owner BIGINT UNIQUE NOT NULL,
+    role_id BIGINT UNIQUE NOT NULL,
+    text_id BIGINT UNIQUE NOT NULL,
+    voice_id BIGINT UNIQUE NOT NULL,
+    created TIMESTAMP DEFAULT (now() at time zone 'utc')
 );
