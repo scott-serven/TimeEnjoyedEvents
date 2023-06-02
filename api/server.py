d4cfd60e3ce10b07e018f1fee1f4337ab427b83c
EvieePy <evieepy@gmail.com>
2023-06-03 01:44:37 +1000

    Add changes to backend server.

--- api/server.py
+++ api/server.py
@@ -28,6 +28,7 @@ import uuid
 from typing import Any
 
 import asyncpg
+import discord
 from starlette.applications import Starlette
 from starlette.requests import Request
 from starlette.responses import JSONResponse, Response
@@ -46,21 +47,31 @@ logger.addHandler(universal.Handler(level=logging_level))
 logger.propagate = False
 
 
+TIMEENJOYED_SERVER: int = 859565527343955998
+
+
 class Server(Starlette):
 
-    def __init__(self) -> None:
+    def __init__(self, *, client: discord.Client) -> None:
+        self.client = client
+
         self.database: universal.Database | None = None
+
         self.commit_queues: dict[str, asyncio.Queue] = {}
+        self.team_feed_queues: dict[str, asyncio.Queue] = {}
 
         routes: list[Route] = [
             Route('/github/commit_feed', self.event_commit, methods=['GET']),
-            Route('/github/{team_id:int}/{team_token:str}', self.receive_github, methods=['POST'])
+            Route('/github/{team_id:int}/{team_token:str}', self.receive_github, methods=['POST']),
+            Route('/teams/feed', self.team_feed, methods=['GET']),
+            Route('/teams/update', self.receive_team_feed_update, methods=['POST']),
+            Route('/teams/feed_event', self.event_team_feed, methods=['GET']),
         ]
 
         super().__init__(
             debug=universal.CONFIG['SERVER']['debug'],
             routes=routes,
-            middleware=[Middleware(CORSMiddleware, allow_origins=['*'])],
+            middleware=[Middleware(CORSMiddleware, allow_origins=['*'], allow_methods=['*'], allow_headers=['*'])],
             on_startup=[self.on_ready]
         )
 
@@ -94,15 +105,17 @@ class Server(Starlette):
         id_: int = request.path_params['team_id']
         token: str = request.path_params['team_token']
 
-        team: asyncpg.Record = await self.database.fetch_team(team_id=id_)
+        team: list[asyncpg.Record] = await self.database.fetch_team(team_id=int(id_))
         if not team:
             return Response(status_code=404)
+
+        team: asyncpg.Record = team[0]
 
         if team['token'] != token:
             return Response(status_code=401)
 
         data: dict[str, Any] = await request.json()
-        to_send: dict[str, Any] = {'team': {'id': team['team_id'], 'name': team['name']}}
+        to_send: dict[str, Any] = {'team': {'name': team['name']}}
 
         sender: dict[str, str] = {'name': data['sender']['login'], 'avatar': data['sender']['avatar_url']}
         commits: list[dict[str, str]] = []
@@ -115,5 +128,74 @@ class Server(Starlette):
 
         for queue in self.commit_queues.values():
             await queue.put(to_send)
+
+        return Response(status_code=200)
+
+    async def get_team_feed(self) -> dict[str | None, list[dict[str, str]]]:
+        guild: discord.Guild = self.client.get_guild(TIMEENJOYED_SERVER)
+        members: list[asyncpg.Record] = await self.database.fetch_members()
+
+        data: dict[str | None, list[dict[str, str]]] = {}
+        for member in members:
+
+            dmember: discord.Member = guild.get_member(member['member_id'])
+            member_data: dict[str, str | bool] = {
+                'name': dmember.display_name,
+                'avatar': dmember.display_avatar.url,
+                'languages': member['languages'],
+                'timezone': member['timezone'].total_seconds() / (60 * 60),
+                'solo': member['solo']
+            }
+
+            try:
+                # name is actually team name...
+                data[member['name']].append(member_data)
+            except KeyError:
+                data[member['name']] = [member_data]
+
+        return data
+
+    async def team_feed(self, request: Request) -> JSONResponse | Response:
+        data: dict[str | None, list[dict[str, str]]] = await self.get_team_feed()
+
+        return JSONResponse(data, status_code=200)
+
+    async def event_team_feed(self, request: Request) -> EventSourceResponse:
+        id_: str = str(uuid.uuid4())
+
+        self.team_feed_queues[id_] = asyncio.Queue()
+        return EventSourceResponse(self.publisher_team_feed(request, id_))
+
+    async def publisher_team_feed(self, request: Request, id_: str, /) -> dict[str, Any]:
+        await self.client.wait_until_ready()
+
+        queue: asyncio.Queue = self.team_feed_queues[id_]
+        await queue.put(await self.get_team_feed())
+
+        while True:
+            try:
+                payload: dict[str | None, list[dict[str, str]]] = await queue.get()
+                yield json.dumps(payload)
+            except asyncio.CancelledError:
+                break
+
+            if await request.is_disconnected():
+                break
+
+        del self.team_feed_queues[id_]
+
+    async def receive_team_feed_update(self, request: Request) -> Response:
+        auth: str = request.headers.get('authorization', None)
+        if not auth:
+            return Response(status_code=401)
+
+        secret: str = universal.CONFIG['TOKENS']['backend']
+        if secret != auth:
+            return Response(status_code=401)
+
+        data: dict[str | None, list[dict[str, str]]] = await self.get_team_feed()
+
+        for queue in self.team_feed_queues.values():
+            await queue.put(data)
 
         return Response(status_code=200)
